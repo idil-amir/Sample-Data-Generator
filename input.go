1eb915df5e483e8f0b2016738ba83da9eb0d16f4
Muhammad Idil Haq Amir <idil.amir@tokopedia.com>
2023-05-11 11:36:11 +0700

    Update data sample

--- input.go
+++ input.go
@@ -1,15 +1,20 @@
 package main
 
-// type Column struct {
-// 	DataSample string `json:"dataSample"`
-// 	TribeOwner string `json:"tribeOwner"`
-// 	IsPII      bool   `json:"isPII"`
-// 	TypePII    string `json:"typePII"`
-// }
+// input column data here
+type Column struct {
+	Column1  Data `json:"id" dataType:"int" tribeOwner:"3" isPII:"false" typePII:""`
+	Column2  Data `json:"quest_user_id" dataType:"int" tribeOwner:"3" isPII:"false" typePII:""`
+	Column3  Data `json:"benefit_type" dataType:"int" tribeOwner:"3" isPII:"false" typePII:""`
+	Column4  Data `json:"benefit_unique_code" dataType:"string" tribeOwner:"3" isPII:"false" typePII:""`
+	Column5  Data `json:"status" dataType:"int" tribeOwner:"3" isPII:"false" typePII:""`
+	Column6  Data `json:"metadata" dataType:"json" tribeOwner:"3" isPII:"false" typePII:""`
+	Column7  Data `json:"created_at" dataType:"int" tribeOwner:"3" isPII:"false" typePII:""`
+	Column8  Data `json:"created_by" dataType:"int" tribeOwner:"3" isPII:"true" typePII:"C3"`
+	Column9  Data `json:"updated_at" dataType:"int" tribeOwner:"3" isPII:"false" typePII:""`
+	Column10 Data `json:"updated_by" dataType:"int" tribeOwner:"3" isPII:"true" typePII:"C3"`
+}
 
-// type Table struct {
-// 	ID          Column `json:"id"            dataType:"int"     tribeOwner:"3" isPII:"true"  typePII:"C3"`
-// 	Channel     Column `json:"channel"       dataType:"string"  tribeOwner:"3" isPII:"false" typePII:""`
-// 	Name        Column `json:"name"          dataType:"string"  tribeOwner:"3" isPII:"false" typePII:""`
-// 	CreatedTime Column `json:"created_time"  dataType:"string"  tribeOwner:"3" isPII:"false" typePII:""`
-// }
+// input table data here
+type Table struct {
+	TableName Column `json:"quest.quest_trx_quest_user_benefit"`
+}
